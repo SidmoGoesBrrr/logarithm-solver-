b52dcd0c24ad52573dfd4bee986cf264cf4ee41a
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-10-14 19:37:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,19 +14,23 @@ operation = st.selectbox("Select operation:", ["Multiplication", "Division"])
 
 if st.button("Calculate"):
     if operation == "Multiplication":
-        result = np.log10(num1) + np.log10(num2)  # Changed + for multiplication
+        result = np.log10(num1) + np.log10(num2)
         log_num1 = np.log10(num1)
         log_num2 = np.log10(num2)
     elif operation == "Division":
-        result = np.log10(num1) - np.log10(num2)  # Changed - for division
+        result = np.log10(num1) - np.log10(num2)
         log_num1 = np.log10(num1)
         log_num2 = np.log10(num2)
 
     operation_str = f"AL(log({num1}) {'+' if operation == 'Multiplication' else '-'} log({num2}))"
     st.write(f"1) {operation_str}")
-    st.write(f"2) AL({log_num1:.3f} {'+' if operation == 'Multiplication' else '-'} {log_num2:.3f})")
-    st.write(f"3) AL({result:.3f})")
-
+    
+    # Split the result into the first digit and the rest of the digits
+    first_digit, rest_of_digits = int(result), result - int(result)
+    
+    st.write(f"2) AL({log_num1:.3f} {'+' if operation == 'Multiplication' else '-'} {log_num2:.3f}")
+    st.write(f"3) AL({first_digit}. {abs(rest_of_digits * 10**3)} x 10³")  # Adjust the power of 10 accordingly
+    
     # Calculate the antilog of the result
     antilog_result = 10 ** result
     st.write(f"4) The actual antilog of the result is: {antilog_result:.3f}")
