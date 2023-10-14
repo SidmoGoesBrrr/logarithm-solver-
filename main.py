2cd0504906480179ca9d4232766e8052e8bb8e84
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-10-14 21:21:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import numpy as np
 
 st.title("Logarithmic Calculation App")
-st.header("Made by Siddhant because completing physics journal is not fun")
+st.header("Made by Siddhant because completing the physics journal is not fun")
+
 # Input for the first number
 num1 = st.number_input("Enter the first number (up to 3 decimal places):", format="%.3f")
 
@@ -21,26 +22,14 @@ if st.button("Calculate"):
         result = np.log10(num1) - np.log10(num2)
         log_num1 = np.log10(num1)
         log_num2 = np.log10(num2)
-    st.text(result)
-    operation_str = f"AL(log({num1}) {'+' if operation == 'Multiplication' else '-'} log({num2})"
+
+    operation_str = f"AL(log({num1}) + log({num2}))"
     st.subheader(f"{num1} {'x' if operation == 'Multiplication' else '%'} {num2}")
-    st.write(f"1) {operation_str})")
-    
-    # Split the result into the first digit and the rest of the digits
-    first_digit, rest_of_digits = str(result).split(".")
-    st.text(first_digit)
-    st.text(rest_of_digits)
-    if operation == 'Multiplication':
-        result1 = log_num1 * log_num2
-        st.write(f"2) AL({log_num1:.3f} {'*'} {log_num2:.3f})")
-        st.write(f"3) AL({result1:.3f})")
-    elif operation == 'Division':
-        result1 = log_num1 / log_num2
-        st.write(f"2) AL({log_num1:.3f} {'/'} {log_num2:.3f})")
-        st.write(f"3) AL({result1:.3f})")
-    st.write(f"4)") 
-    
-    # Calculate the antilog of the result
-    antilog_result = 10 ** result
-    st.write(f"5) The actual antilog of the result is: {antilog_result:.3f}")
-    
+    st.write(f"1) {operation_str}")
+
+    result1 = log_num1 + log_num2
+    st.write(f"2) AL({log_num1:.3f} + {log_num2:.3f})")
+    st.write(f"3) AL({result1:.3f})")
+
+    st.write(f"4) The antilog of the result is approximately: {10 ** result:.3f}")
+    st.write(f"5) The actual antilog of the result is: {10 ** result:.3f}")
