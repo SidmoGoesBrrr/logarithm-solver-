61f5027e0e1336a4dc851a9abd25e47e63c23980
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-10-14 19:41:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,16 +22,17 @@ if st.button("Calculate"):
         log_num1 = np.log10(num1)
         log_num2 = np.log10(num2)
 
-    operation_str = f"AL(log({num1}) {'+' if operation == 'Multiplication' else '-'} log({num2}))"
-    st.write(f"1) {operation_str}")
+    operation_str = f"AL(log({num1}) {'+' if operation == 'Multiplication' else '-'} log({num2})"
+    st.write(f"1) {operation_str})")
     
     # Split the result into the first digit and the rest of the digits
-    first_digit, rest_of_digits = int(result), result - int(result)
+    first_digit, rest_of_digits = str(result).split(".")
     
-    st.write(f"2) AL({log_num1:.3f} {'+' if operation == 'Multiplication' else '-'} {log_num2:.3f}")
-    st.write(f"3) AL({first_digit}. {abs(rest_of_digits * 10**3)} x 10³")  # Adjust the power of 10 accordingly
+    st.write(f"2) AL({log_num1:.3f} {'+' if operation == 'Multiplication' else '-'} {log_num2:.3f})")
+    st.write(f"3) AL({first_digit}.{rest_of_digits[1:]} x 10^{len(rest_of_digits) - 1}")
     
     # Calculate the antilog of the result
     antilog_result = 10 ** result
     st.write(f"4) The actual antilog of the result is: {antilog_result:.3f}")
+    st.write(f"5) {first_digit}.{rest_of_digits[1:]} x 10^{len(rest_of_digits) - 1}")
     
